eb9d23b8ae1958854bc90ce4d91b3338cab90e42
De Almeida Florian <florian.de-almeida@etu.u-bordeaux.fr>
2018-01-18 09:19:25 +0100

    First activity design

--- app/src/main/java/lpdawin/fr/taquin/MainActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/MainActivity.java
@@ -2,13 +2,31 @@ package lpdawin.fr.taquin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class
 MainActivity extends Activity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+    public void checkImage(View view){
+        final Button button = (Button) findViewById(view.getId());
+
+        Log.d("Test", button.getText() + "");
+    }
+
+
 }
+

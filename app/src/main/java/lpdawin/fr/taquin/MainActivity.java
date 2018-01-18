39253c21bfaec6567176a7b8e670e74ea19f5006
De Almeida Florian <florian.de-almeida@etu.u-bordeaux.fr>
2018-01-18 09:31:31 +0100

    First test

--- app/src/main/java/lpdawin/fr/taquin/MainActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/MainActivity.java
@@ -2,9 +2,13 @@ package lpdawin.fr.taquin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class
 MainActivity extends Activity {
@@ -23,8 +27,24 @@ MainActivity extends Activity {
 
     public void checkImage(View view){
         final Button button = (Button) findViewById(view.getId());
-
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         Log.d("Test", button.getText() + "");
+
+        if (radioGroup.getCheckedRadioButtonId() == -1)
+        {
+            // no radio buttons are checked
+            Toast toast = Toast.makeText(getApplicationContext(), "Veuillez sélectionner une image", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else
+        {
+            // one of the radio buttons is checked
+            RadioButton rb = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
+            Log.d("Test", rb.getText() + "");
+
+
+        }
+
     }
 
 

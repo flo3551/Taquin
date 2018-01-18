5ade7c1ad06b313a6c6078ceb2af50df1211afc9
vcarrasco8 <carrascovincent8@gmail.com>
2018-01-18 11:35:14 +0100

    Open gameActivity with extras

--- app/src/main/java/lpdawin/fr/taquin/GameActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/GameActivity.java
@@ -2,16 +2,27 @@ package lpdawin.fr.taquin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
 
 public class GameActivity extends Activity {
 
+    private String pathImage;
+    private String tailleGrille;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+
+        pathImage = this.getIntent().getStringExtra("pathImage");
+        tailleGrille = this.getIntent().getStringExtra("tailleGrille");
+
+        Log.d("Test taille", tailleGrille);
+        Log.d("Test path", pathImage);
+
     }
 
 }
--- app/src/main/java/lpdawin/fr/taquin/MainActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/MainActivity.java
@@ -1,6 +1,7 @@
 package lpdawin.fr.taquin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -10,46 +11,45 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import static android.R.attr.path;
+
 public class
 MainActivity extends Activity {
 
-
-
+    private String pathImage;
+    private String tailleGrille;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
 
     public void checkImage(View view){
         final Button button = (Button) findViewById(view.getId());
         final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        Log.d("Test", button.getText() + "");
 
-        if (radioGroup.getCheckedRadioButtonId() == -1)
-        {
+        tailleGrille = button.getText().charAt(0) + "";
+
+
+        if (radioGroup.getCheckedRadioButtonId() == -1) {
             // no radio buttons are checked
             Toast toast = Toast.makeText(getApplicationContext(), "Veuillez sélectionner une image", Toast.LENGTH_LONG);
             toast.show();
-        }
-        else
-        {
+        } else {
             // one of the radio buttons is checked
             RadioButton rb = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
-            Log.d("Test", rb.getText() + "");
-
+            pathImage = rb.getText().charAt(rb.getText().length()-1) + "";
             // Start activity
             // Put extra
             // 1 = tailleGrille
             // 2 = Path image
+
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("tailleGrille", tailleGrille);
+            intent.putExtra("pathImage", pathImage);
+            startActivity(intent);
         }
-
     }
-
-
 }
 

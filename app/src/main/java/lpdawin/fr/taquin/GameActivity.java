4346e245fbec5d8c0644382f9a6e39af005f3470
vcarrasco8 <carrascovincent8@gmail.com>
2018-01-18 09:30:29 +0100

    Add game activity

---
+++ app/src/main/java/lpdawin/fr/taquin/GameActivity.java
@@ -0,0 +1,13 @@
+package lpdawin.fr.taquin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}

0f31f6d3d985262f2ca766af73b5acab7940e035
De Almeida Florian <florian.de-almeida@etu.u-bordeaux.fr>
2018-01-18 12:17:37 +0100

    image adapter

--- app/src/main/java/lpdawin/fr/taquin/GameActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/GameActivity.java
@@ -1,6 +1,10 @@
 package lpdawin.fr.taquin;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
@@ -11,18 +15,36 @@ public class GameActivity extends Activity {
 
     private String pathImage;
     private String tailleGrille;
-
+    private Bitmap image;
+    private int taille;
+    private ImageAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+
+
         pathImage = this.getIntent().getStringExtra("pathImage");
         tailleGrille = this.getIntent().getStringExtra("tailleGrille");
+        taille = Integer.parseInt(tailleGrille);
 
-        Log.d("Test taille", tailleGrille);
-        Log.d("Test path", pathImage);
 
+        switch (pathImage){
+            case "1":
+                image = BitmapFactory.decodeResource(this.getResources(), R.drawable.jonsnow);
+                break;
+            case "2":
+                image = BitmapFactory.decodeResource(this.getResources(), R.drawable.mrbean);
+                break;
+            case "3":
+                image = BitmapFactory.decodeResource(this.getResources(), R.drawable.rick);
+                break;
+        }
+        GridView leGV = (GridView) findViewById(R.id.leGridView);
+        adapter = new ImageAdapter(getBaseContext(), image, taille);
+        leGV.setAdapter(adapter);
+        leGV.setNumColumns(taille);
     }
 
 }

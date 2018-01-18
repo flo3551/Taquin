8711c093c0bad66e7d8dab65a9a8b2c07212c165
vcarrasco8 <carrascovincent8@gmail.com>
2018-01-18 11:31:48 +0100

    Open gameActivity with extras

--- app/src/main/java/lpdawin/fr/taquin/GameActivity.java
+++ app/src/main/java/lpdawin/fr/taquin/GameActivity.java
@@ -2,12 +2,16 @@ package lpdawin.fr.taquin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
 
 public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_main);
     }
+
 }
